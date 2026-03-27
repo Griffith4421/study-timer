0b7b9b7a96308363085f86017e9d84bd6459c9d3
Griffith4421 <thunderstorm9526@gmail.com>
2026-03-27 13:23:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let time = 1500;
+let timer;
+
+function updateDisplay() {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  document.getElementById("time").innerText =
+    `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+function startTimer() {
+  if (timer) return;
+
+  timer = setInterval(() => {
+    if (time > 0) {
+      time--;
+      updateDisplay();
+    }
+  }, 1000);
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  timer = null;
+  time = 1500;
+  updateDisplay();
+}
+
+updateDisplay();
